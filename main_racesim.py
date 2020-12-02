ae9c671ebe5c3c052be1eb9dbb36d688c0b2603f
Vadim <vad99lord@gmail.com>
2020-12-02 19:42:44 +0300

    Update main_racesim.py

--- main_racesim.py
+++ main_racesim.py
@@ -28,7 +28,7 @@ the simulation is located at the bottom. Have a look there to insert the require
 repo_path_ = os.path.dirname(os.path.abspath(__file__))
 
 # read dependencies from requirements.txt
-requirements_path = os.path.join(repo_path_, 'requirements.txt')
+""" requirements_path = os.path.join(repo_path_, 'requirements.txt')
 dependencies = []
 
 with open(requirements_path, 'r') as fh_:
@@ -39,7 +39,7 @@ with open(requirements_path, 'r') as fh_:
         line = fh_.readline()
 
 # check dependencies
-pkg_resources.require(dependencies)
+pkg_resources.require(dependencies)"""
 
 
 # ----------------------------------------------------------------------------------------------------------------------
